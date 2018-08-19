8e75a0059900d89aa2d05e1fccdc27cb705a2578
haoming37 <nh.ornis@gmail.com>
2018-08-19 23:56:43 +0900

    update setup_dotfile.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -4,10 +4,8 @@ script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 for x in $(ls -a|grep -i "^\.[a-z]"|grep -v .git|sed -e '$abin')
 do
     if [ -e ~/${x} ]; then
-        if [ -L ~/${x} ]; then
+        if [ ! -L ~/${x} ]; then
             mv ~/${x} ~/${x}_old 
-        elif [ -d ~/${x} ]; then
-            mv ~/${x} ~/${x}_old
         else
             rm ~/${x}
         fi
@@ -15,6 +13,8 @@ do
     ln -s $script_dir/${x} ~/${x}
 done
 #install dein
+echo "export VIMRUNTIME=$script_dir/vim_runtime" >>~/.bashrc$
+export VIMRUNTIME=$script_dir/vim_runtime
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 rm installer.sh
