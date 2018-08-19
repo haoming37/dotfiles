60f87f2aa22a63864df389378064ea964a306fed
haoming37 <nh.ornis@gmail.com>
2018-08-19 22:54:06 +0900

    update vim

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#set symbolic link
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 for x in $(ls -a|grep -i "^\.[a-z]"|grep -v .git)
 do
@@ -11,3 +12,7 @@ do
     fi
     ln -s $script_dir/${x} ~/${x}
 done
+#install dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+rm installer.sh
---
+++ setup_vim.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/vim/vim 
+cd vim
+./configure
+make
+cp src/vim ../bin/
