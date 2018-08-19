06c120664bbb443a28d81778cb908ebcf4b98669
haoming37 <nh.ornis@gmail.com>
2018-08-19 23:40:03 +0900

    update setup_dotfiles.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set symbolic link
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-for x in $(ls -a|grep -i "^\.[a-z]"|grep -v .git)
+for x in $(ls -a|grep -i "^\.[a-z]"|grep -v .git|sed -e '$abin')
 do
     if [ -e ~/${x} ]; then
         if [ -L ~/${x} ]; then
@@ -16,3 +16,4 @@ done
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 rm installer.sh
+$script_dir/bin/vim -e +norm':call dein#install()' +q!
