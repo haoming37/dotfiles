f47752acaf352d7b82c93622c38580eb1d3f6893
haoming37 <nh.ornis@gmail.com>
2018-08-19 21:18:32 +0900

    update setup_global.sh

--- setup_global.sh
+++ setup_global.sh
@@ -3,10 +3,10 @@ curl -L http://tamacom.com/global/global-6.6.2.tar.gz|tar zx -C ./
 cd global-6.6.2
 ./configure
 make
-if [ -e ~/bin ]; then
+if [ -d ~/bin ]; then
    cp gtags/gtags ~/bin
    cp global/global ~/bin
 fi
-if [ -e ~/.vim/plugin ]; then
+if [ -d ~/.vim/plugin ]; then
     cp gtags.vim ~/.vim/plugin
 fi
