cfb42b8fdc6b6496ad911d01c49ef5dbf654ed5e
haoming37 <nh.ornis@gmail.com>
2018-08-19 23:58:52 +0900

    update setup_dotfile.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -13,7 +13,7 @@ do
     ln -s $script_dir/${x} ~/${x}
 done
 #install dein
-echo "export VIMRUNTIME=$script_dir/vim_runtime" >>~/.bashrc$
+echo "export VIMRUNTIME=$script_dir/vim_runtime" >>~/.bashrc
 export VIMRUNTIME=$script_dir/vim_runtime
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
